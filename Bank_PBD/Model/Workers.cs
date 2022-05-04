6e0a38a148fb11ce4815c16900a9bca31a8ea77b
PioSwi00 <piotswi950@student.polsl.pl>
2022-05-04 19:33:04 +0200

    pracownicy

---
+++ Bank_PBD/Model/Workers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_PBD.Model
+{
+    [Table ("Workers")]
+    public class Workers
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required,MaxLength(40)]
+        public string NameOfWorker { get; set; }
+        [Required, MaxLength(40)]
+        public string SurnameOfWorker { get; set; }
+    }
+}
