5dbf92defcef6d8e77c9726f1a41a4f424b6f341
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2022-05-04 19:11:29 +0200

    Dodaj pliki projektów.

---
+++ Bank_PBD/Model/DbBankContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_PBD.Model
+{
+    public class DbBankContext : DbContext
+    {
+        public DbBankContext() 
+            : base(@"Data Source=(localdb)\MSSQLLocalDB;
+                    Initial Catalog=Bank;Integrated Security=True;
+                    Connect Timeout=30;Encrypt=False;
+                    TrustServerCertificate=False;
+                    ApplicationIntent=ReadWrite;
+                    MultiSubnetFailover=False")
+        { }
+    }
+}
