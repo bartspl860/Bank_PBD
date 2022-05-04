1e580aa4b8b7ca1a9f299d661a9e5eedb2e59d4c
PioSwi00 <piotswi950@student.polsl.pl>
2022-05-04 20:39:31 +0200

    payin/out

---
+++ Bank_PBD/Model/Payins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_PBD.Model
+{
+    [Table("Deposits")]
+    public class Payins
+    {
+        [Key]
+        public int Id { get; set; } 
+        
+        public decimal PayinsDepositAmount { get; set; }  
+
+        public int AccountId { get; set; }  
+        public DateTime Payins_date { get; set; }
+
+
+
+
+    }
+}
