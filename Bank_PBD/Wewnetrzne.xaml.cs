7031b9b8d53a4bcede96adeacf0f3d66b98f81e6
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2022-05-20 14:25:03 +0200

    Transakcje podpięte pod Gui

--- Bank_PBD/Wewnetrzne.xaml.cs
+++ Bank_PBD/Wewnetrzne.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Bank_PBD.Storage;
 using Bank_PBD.Model;
+using Bank_PBD.Actions;
 
 namespace Bank_PBD
 {
@@ -23,7 +24,11 @@ namespace Bank_PBD
     /// </summary>
     public partial class Wewnetrzne : Page
     {
+        private Dictionary<string, bool> conditions = new Dictionary<string, bool>();
         private Account SelectedAccount { get; set; }
+        private string SelectedTitle { get; set; }
+        private decimal SelectedSum { get; set; }
+        private string SelectedIban { get; set; }
         public Wewnetrzne()
         {
             InitializeComponent();
@@ -32,12 +37,29 @@ namespace Bank_PBD
             {
                 lbxAccounts.Items.Add(account);
             }
+
+            conditions.Add("Account", false);
+            conditions.Add("Sum", false);
+            conditions.Add("Title", false);
+            conditions.Add("IBAN", false);
+        }
+        private void checkConditions()
+        {
+            foreach (var condition in conditions) { 
+                if (!condition.Value)
+                {
+                    btnSubmitTransaction.IsEnabled = false;
+                    return;
+                }                
+            }
+            btnSubmitTransaction.IsEnabled = true;
         }
         private void UpdateBalanceAfterTransaction()
         {
             try
             {
                 var sum = Convert.ToDecimal(tbxSum.Text);
+                SelectedSum = sum;
 
                 if (SelectedAccount != null)
                 {
@@ -48,32 +70,74 @@ namespace Bank_PBD
                     else
                         lblBalanceAfterTransaction.Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255));
                     lblBalanceAfterTransaction.Content = $"Stan konta po transakcji: {afterTransaction}";
-                    
                 }                   
             }
             catch (Exception ex)
             {
+                conditions["Sum"] = false;
+                checkConditions();
                 return;
             }
+
+            conditions["Sum"] = true;
+
+            checkConditions();
         }
         private void lbxAccounts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (lbxAccounts.SelectedIndex < 0)
+            {
+                conditions["Account"] = false;
                 return;
-
+            }
+                
             SelectedAccount = Session.Accounts[lbxAccounts.SelectedIndex];
 
             lblSelectedAccount.Content = 
                 $"Numer IBAN: {SelectedAccount.IBAN_Number} \n" +
                 $"Nazwa: {SelectedAccount.Name} \n" +
-                $"Stan Konta: {SelectedAccount.Balance} \n";      
+                $"Stan Konta: {SelectedAccount.Balance} \n";
 
+            conditions["Account"] = true;
             UpdateBalanceAfterTransaction();
+            checkConditions();
         }
 
         private void tbxSum_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateBalanceAfterTransaction();                        
+        }
+
+        private void tbxTitle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (tbxTitle.Text == string.Empty)
+            {
+                conditions["Title"] = false;
+                checkConditions();
+                return;
+            }
+            conditions["Title"] = true;
+            SelectedTitle = tbxTitle.Text;
+            checkConditions();
+        }
+
+        private void tbxIBAN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (tbxIBAN.Text == string.Empty)
+            {
+                conditions["IBAN"] = false;
+                checkConditions();
+                return;
+            }
+            conditions["IBAN"] = true;
+            SelectedIban = tbxIBAN.Text;
+            checkConditions();
+        }
+        private void btnSubmitTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            Transaction.Make(SelectedAccount.IBAN_Number, SelectedIban, SelectedSum, SelectedTitle);
         }
     }
 }
