06bd798686e8a309ef81efc3f933d19dc9674bf6
PioSwi00 <piotswi950@student.polsl.pl>
2022-05-25 17:53:44 +0200

    AccMenag

---
+++ Bank_PBD/AccMenag.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bank_PBD
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AccMenag.xaml
+    /// </summary>
+    public partial class AccMenag : Page
+    {
+        public AccMenag()
+        {
+            InitializeComponent();
+        }
+    }
+}
