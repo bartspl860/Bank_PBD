c5907f69ac26511d34b09b57f554cbaeba0966bb
PioSwi00 <piotswi950@student.polsl.pl>
2022-05-25 18:14:32 +0200

    Edycja Konta

--- Bank_PBD/AccMenag.xaml.cs
+++ Bank_PBD/AccMenag.xaml.cs
@@ -24,5 +24,15 @@ namespace Bank_PBD
         {
             InitializeComponent();
         }
+
+        private void btnQuestiomMark_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Wybierz z listy konto które chcesz usunąć.\nPAMIĘTAJ konto nie może mieć dostępnych środków");
+        }
+
+        private void btnAddAcc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
